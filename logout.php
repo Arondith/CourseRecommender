52df794845f99d65aeee2ecb5ff1bd89d3e17be7
Arondite <Aronditee@gmail.com>
2026-09-19 16:14:09 +0800

    Harden logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,24 @@
 <?php
-// logout.php
-session_start();
-session_unset();
+declare(strict_types=1);
+
+require_once __DIR__ . '/session_bootstrap.php';
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
+
 session_destroy();
+
+header('Content-Type: application/json; charset=utf-8');
 echo json_encode(['success' => true]);
-?>
